98b668fe1ab5d542e2c21d7924bb3d481bdfaec2
CodeDoctorDE <janandrejdiehl@gmail.com>
2021-10-10 15:12:07 +0200

    add listview to home page

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -20,6 +20,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<AppDocument> _documents = [];
+  bool gridView = true;
 
   @override
   void initState() {
@@ -44,6 +45,9 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: const Text("Butterfly"), actions: [
         IconButton(
+            icon: Icon(gridView ? PhosphorIcons.listLight : PhosphorIcons.gridFourLight),
+            onPressed: () => setState(() => gridView = !gridView)),
+        IconButton(
           icon: const Icon(PhosphorIcons.folderOpenLight),
           tooltip: AppLocalizations.of(context)!.open,
           onPressed: () {
@@ -65,67 +69,7 @@ class _HomePageState extends State<HomePage> {
             tooltip: AppLocalizations.of(context)?.settings,
             onPressed: () => Modular.to.pushNamed("/settings/"))
       ]),
-      body: SingleChildScrollView(
-          child: Align(
-              alignment: Alignment.topCenter,
-              child: ConstrainedBox(
-                  constraints: const BoxConstraints(minWidth: 100, maxWidth: 1000),
-                  child: Wrap(
-                      alignment: WrapAlignment.center,
-                      children: List.generate(_documents.length, (index) {
-                        var document = _documents[index];
-                        return Card(
-                            margin: const EdgeInsets.all(5),
-                            child: Column(
-                              children: [
-                                InkWell(
-                                    onTap: () {
-                                      Modular.to.pushNamed("/pad/$index").then((value) {
-                                        if (mounted) loadDocuments();
-                                      });
-                                    },
-                                    child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 20, vertical: 10),
-                                        child: Column(children: [
-                                          Text(document.name,
-                                              style: Theme.of(context).textTheme.headline6),
-                                          Text(document.description,
-                                              style: Theme.of(context).textTheme.overline)
-                                        ]))),
-                                IconButton(
-                                  icon: const Icon(PhosphorIcons.trashLight),
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                              title: Text(AppLocalizations.of(context)!.areYouSure),
-                                              content:
-                                                  Text(AppLocalizations.of(context)!.reallyDelete),
-                                              actions: [
-                                                TextButton(
-                                                  child: Text(AppLocalizations.of(context)!
-                                                      .no
-                                                      .toUpperCase()),
-                                                  onPressed: () => Navigator.of(context).pop(),
-                                                ),
-                                                TextButton(
-                                                  child: Text(AppLocalizations.of(context)!
-                                                      .yes
-                                                      .toUpperCase()),
-                                                  onPressed: () {
-                                                    setState(() => _documents.removeAt(index));
-                                                    saveDocuments();
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                )
-                                              ],
-                                            ));
-                                  },
-                                )
-                              ],
-                            ));
-                      }))))),
+      body: gridView ? _buildGridView() : _buildListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(PhosphorIcons.plusLight),
         onPressed: () {
@@ -156,4 +100,74 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  Widget _buildListView() => ListView.builder(
+        itemCount: _documents.length,
+        itemBuilder: (context, index) {
+          var document = _documents[index];
+          return ListTile(
+              title: Text(document.name),
+              onTap: () => _openDocument(index),
+              trailing: IconButton(
+                icon: const Icon(PhosphorIcons.trashLight),
+                onPressed: () => _deleteDialog(index),
+              ));
+        },
+      );
+
+  void _openDocument(int index) => Modular.to.pushNamed("/pad/$index").then((value) {
+        if (mounted) loadDocuments();
+      });
+  void _deleteDialog(int index) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: Text(AppLocalizations.of(context)!.areYouSure),
+            content: Text(AppLocalizations.of(context)!.reallyDelete),
+            actions: [
+              TextButton(
+                child: Text(AppLocalizations.of(context)!.no.toUpperCase()),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: Text(AppLocalizations.of(context)!.yes.toUpperCase()),
+                onPressed: () {
+                  setState(() => _documents.removeAt(index));
+                  saveDocuments();
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          ));
+
+  Widget _buildGridView() => SingleChildScrollView(
+      child: Align(
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: 100, maxWidth: 1000),
+              child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: List.generate(_documents.length, (index) {
+                    var document = _documents[index];
+                    return Card(
+                      margin: const EdgeInsets.all(5),
+                      child: Column(
+                        children: [
+                          InkWell(
+                              onTap: () => _openDocument(index),
+                              child: Container(
+                                  width: 300,
+                                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                  child: Column(children: [
+                                    Text(document.name,
+                                        style: Theme.of(context).textTheme.headline6),
+                                    Text(document.description,
+                                        style: Theme.of(context).textTheme.overline)
+                                  ]))),
+                          IconButton(
+                              icon: const Icon(PhosphorIcons.trashLight),
+                              onPressed: () => _deleteDialog(index))
+                        ],
+                      ),
+                    );
+                  })))));
 }
