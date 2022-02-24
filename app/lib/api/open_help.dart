93074f652743abb47d1d8bee34e8e24ac6d1517d
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-24 19:49:41 +0100

    Fix label and help

--- app/lib/api/open_help.dart
+++ app/lib/api/open_help.dart
@@ -4,5 +4,5 @@ Future<bool> openHelp(List<String> pageLocation) {
   return launch(Uri(
       scheme: 'https',
       host: 'docs.butterfly.linwood.dev',
-      pathSegments: ['docs', '1.1', ...pageLocation]).toString());
+      pathSegments: ['docs', '1.3', ...pageLocation]).toString());
 }
