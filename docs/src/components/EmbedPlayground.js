698a135875e8f4b65107e29371b2f6398af919f1
CodeDoctor <wusel905@gmail.com>
2022-05-01 22:01:02 +0200

    Move embed playground logic in own file

---
+++ docs/src/components/EmbedPlayground.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+export default function EmbedPlayground() {
+  const [host, setHost] = useState("https://butterfly.linwood.dev")
+  const [save, setSave] = useState(true)
+  const [editable, setEditable] = useState(true)
+  const [theme, setTheme] = useState("user")
+  const [language, setLanguage] = useState("user")
+  const url = `${host}/embed?save=${save}&editable=${editable}&theme=${theme}&language=${language}`;
+  return (
+    <div>
+      <div className="input-group">
+        <label htmlFor="language">Host</label>
+        <input className="input" type="text" value={host} id="language" onChange={(e) => setHost(e.target.value)} />
+      </div>
+      <label className="checkbox">Save
+        <input type="checkbox" checked={save} id="save" onChange={() => setSave(!save)} />
+        <span className="checkbox-checkmark"></span>
+      </label>
+      <label className="checkbox">Editable
+        <input type="checkbox" checked={editable} id="editable" onChange={() => setEditable(!editable)} />
+        <span className="checkbox-checkmark"></span>
+      </label>
+      <div className="input-group">
+        <label htmlFor="theme">Theme</label>
+        <input className="input" type="text" value={theme} id="theme" onChange={(e) => setTheme(e.target.value)} />
+      </div>
+      <div className="input-group">
+        <label htmlFor="language">Language</label>
+        <input className="input" type="text" value={language} id="language" onChange={(e) => setLanguage(e.target.value)} />
+      </div>
+      <h2 className="padding-top--lg padding-bottom--sm">Preview</h2>
+      <iframe src={url} width="100%" height="500px" allowtransparency="true"></iframe>
+      <div className="input-group">
+        <label htmlFor="url">Generated url</label>
+        <input className="input" readonly type="text" value={url} id="url" />
+      </div>
+    </div>
+  )
+}
