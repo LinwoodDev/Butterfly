0f9ab7367ee27f5ea4834f48a822f867db84b038
CodeDoctorDE <wusel905@gmail.com>
2022-05-05 08:48:31 +0200

    Fix pdf import missing padding

--- app/lib/dialogs/pages.dart
+++ app/lib/dialogs/pages.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 import '../widgets/header.dart';
 
@@ -31,48 +32,70 @@ class _PagesDialogState extends State<PagesDialog> {
         mainAxisSize: MainAxisSize.min,
         children: [
           Header(
-            title: Text(
-                AppLocalizations.of(context)!.countPages(widget.pages.length)),
+            title:
+                Text(AppLocalizations.of(context)!.countPages(selected.length)),
+            actions: [
+              IconButton(
+                  tooltip: AppLocalizations.of(context)!.invertSelection,
+                  icon: const Icon(PhosphorIcons.selectionInverseLight),
+                  onPressed: () {
+                    setState(() {
+                      // Remove all selected pages and add all other pages.
+                      selected = List.generate(widget.pages.length, (i) => i)
+                          .toSet()
+                          .difference(selected.toSet())
+                          .toList();
+                    });
+                  }),
+            ],
           ),
           Flexible(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
               child: ListView.builder(
-            itemCount: widget.pages.length,
-            itemBuilder: (context, index) {
-              // Show border for selected pages
-              final border = selected.contains(index)
-                  ? Border.all(
-                      width: 4,
-                      color: Theme.of(context).primaryColor,
-                    )
-                  : null;
-              return InkWell(
-                onTap: () {
-                  setState(() {
-                    selected = selected.contains(index)
-                        ? selected.where((e) => e != index).toList()
-                        : [...selected, index];
-                  });
+                itemCount: widget.pages.length,
+                itemBuilder: (context, index) {
+                  // Show border for selected pages
+                  final border = selected.contains(index)
+                      ? Border.all(
+                          width: 4,
+                          color: Theme.of(context).primaryColor,
+                        )
+                      : null;
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        selected = selected.contains(index)
+                            ? selected.where((e) => e != index).toList()
+                            : [...selected, index];
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(border: border),
+                      child: Image.memory(widget.pages[index]),
+                    ),
+                  );
                 },
-                child: Container(
-                  margin: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(border: border),
-                  child: Image.memory(widget.pages[index]),
-                ),
-              );
-            },
+              ),
+            ),
           )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                child: Text(AppLocalizations.of(context)!.cancel),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-              TextButton(
-                child: Text(AppLocalizations.of(context)!.ok),
-                onPressed: () => Navigator.of(context).pop(selected),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  child: Text(AppLocalizations.of(context)!.cancel),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                TextButton(
+                  child: Text(AppLocalizations.of(context)!.ok),
+                  onPressed: () => Navigator.of(context).pop(selected),
+                ),
+              ],
+            ),
           ),
         ],
       ),
