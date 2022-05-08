f91d7c05d4bb88da2b4aa3ff4ef3db6528c4739b
CodeDoctorDE <wusel905@gmail.com>
2022-05-08 16:04:58 +0200

    Fixing safe area bug in settings

--- app/lib/settings/home.dart
+++ app/lib/settings/home.dart
@@ -37,182 +37,195 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: widget.isDialog ? Colors.transparent : null,
-      child: LayoutBuilder(builder: (context, constraints) {
-        final isMobile = constraints.maxWidth < 600;
-        var navigation = Column(children: [
-          Header(
-            title: Text(AppLocalizations.of(context)!.settings),
-            leading: IconButton(
-              icon: const Icon(PhosphorIcons.xLight),
-              onPressed: () => Navigator.of(context).pop(),
+    return SafeArea(
+      child: Material(
+        color: widget.isDialog ? Colors.transparent : null,
+        child: LayoutBuilder(builder: (context, constraints) {
+          final isMobile = constraints.maxWidth < 600;
+          var navigation = Column(children: [
+            Header(
+              title: Text(AppLocalizations.of(context)!.settings),
+              leading: IconButton(
+                icon: const Icon(PhosphorIcons.xLight),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
             ),
-          ),
-          Flexible(
-            child: ListView(
-                controller: _scrollController,
-                shrinkWrap: true,
-                children: [
-                  ListTile(
-                    title: Text(AppLocalizations.of(context)!.data),
-                    leading: const Icon(PhosphorIcons.databaseLight),
-                    selected: widget.isDialog && !isMobile
-                        ? _view == SettingsView.data
-                        : false,
-                    onTap: () {
-                      if (isMobile) {
-                        Navigator.of(context).pop();
-                        GoRouter.of(context).go('/settings/data');
-                      } else {
-                        setState(() {
-                          _view = SettingsView.data;
-                        });
-                      }
-                    },
-                  ),
-                  ListTile(
-                    title: Text(AppLocalizations.of(context)!.behaviors),
-                    leading: const Icon(PhosphorIcons.fadersLight),
-                    selected: widget.isDialog && !isMobile
-                        ? _view == SettingsView.behaviors
-                        : false,
-                    onTap: () {
-                      if (isMobile) {
-                        Navigator.of(context).pop();
-                        GoRouter.of(context).go('/settings/behaviors');
-                      } else {
-                        setState(() {
-                          _view = SettingsView.behaviors;
-                        });
-                      }
-                    },
-                  ),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.monitorLight),
-                      title:
-                          Text(AppLocalizations.of(context)!.personalization),
-                      selected: widget.isDialog && !isMobile
-                          ? _view == SettingsView.personalization
-                          : false,
-                      onTap: () {
-                        if (isMobile) {
-                          Navigator.of(context).pop();
-                          GoRouter.of(context).go('/settings/personalization');
-                        } else {
-                          setState(() {
-                            _view = SettingsView.personalization;
-                          });
-                        }
-                      }),
-                  const Divider(),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.articleLight),
-                      title: Text(AppLocalizations.of(context)!.documentation),
-                      onTap: () => launchUrl(
-                          Uri.https('docs.butterfly.linwood.dev', ''))),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.flagLight),
-                      title: Text(AppLocalizations.of(context)!.releaseNotes),
-                      onTap: () => openReleaseNotes()),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.usersLight),
-                      title: const Text('Discord'),
-                      onTap: () =>
-                          launchUrl(Uri.https('go.linwood.dev', 'discord'))),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.translateLight),
-                      title: const Text('Crowdin'),
-                      onTap: () => launchUrl(
-                          Uri.https('go.linwood.dev', 'butterfly/crowdin'))),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.codeLight),
-                      title: Text(AppLocalizations.of(context)!.source),
-                      onTap: () => launchUrl(
-                          Uri.https('go.linwood.dev', 'butterfly/source'))),
-                  ListTile(
-                      leading:
-                          const Icon(PhosphorIcons.arrowCounterClockwiseLight),
-                      title: Text(AppLocalizations.of(context)!.changelog),
-                      onTap: () => launchUrl(Uri.https(
-                          'docs.butterfly.linwood.dev', 'changelog'))),
-                  const Divider(),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.stackLight),
-                      title: Text(AppLocalizations.of(context)!.license),
-                      onTap: () => launchUrl(
-                          Uri.https('go.linwood.dev', 'butterfly/license'))),
-                  ListTile(
-                      leading:
-                          const Icon(PhosphorIcons.identificationCardLight),
-                      title: Text(AppLocalizations.of(context)!.imprint),
-                      onTap: () =>
-                          launchUrl(Uri.https('go.linwood.dev', 'impress'))),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.shieldLight),
-                      title: Text(AppLocalizations.of(context)!.privacypolicy),
-                      onTap: () => launchUrl(Uri.https(
-                          'docs.butterfly.linwood.dev', 'privacypolicy'))),
-                  ListTile(
-                      leading: const Icon(PhosphorIcons.infoLight),
-                      title: Text(AppLocalizations.of(context)!.information),
-                      onTap: () => PackageInfo.fromPlatform().then((info) =>
-                          showAboutDialog(
-                              context: context,
-                              children: [
-                                ElevatedButton(
-                                    child: Text(AppLocalizations.of(context)!
-                                        .copyVersion),
-                                    onPressed: () => Clipboard.setData(
-                                        ClipboardData(text: info.version))),
-                              ],
-                              applicationVersion: info.version,
-                              applicationIcon:
-                                  Image.asset('images/logo.png', height: 50)))),
-                  if (kIsWeb)
-                    Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: InkWell(
-                            onTap: () => launchUrl(Uri.https('vercel.com', '', {
-                                  'utm_source': 'Linwood',
-                                  'utm_campaign': 'oss'
-                                })),
-                            child: Material(
-                              child: SizedBox(
-                                  height: 50,
-                                  child: SvgPicture.asset(
-                                      'images/powered-by-vercel.svg',
-                                      placeholderBuilder:
-                                          (BuildContext context) => Container(
-                                              padding:
-                                                  const EdgeInsets.all(30.0),
-                                              child:
-                                                  const CircularProgressIndicator()),
-                                      semanticsLabel: 'Powered by Vercel')),
-                            )))
-                ]),
-          )
-        ]);
-        if (isMobile) {
-          return navigation;
-        }
-        Widget content;
-        switch (_view) {
-          case SettingsView.data:
-            content = const DataSettingsPage(inView: true);
-            break;
-          case SettingsView.behaviors:
-            content = const BehaviorsSettingsPage(inView: true);
-            break;
-          case SettingsView.personalization:
-            content = const PersonalizationSettingsPage(inView: true);
-            break;
-        }
-        return Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          SizedBox(width: 300, child: navigation),
-          Expanded(child: content),
-        ]);
-      }),
+            Flexible(
+              child: Material(
+                child: ListView(
+                    controller: _scrollController,
+                    shrinkWrap: true,
+                    children: [
+                      ListTile(
+                        title: Text(AppLocalizations.of(context)!.data),
+                        leading: const Icon(PhosphorIcons.databaseLight),
+                        selected: widget.isDialog && !isMobile
+                            ? _view == SettingsView.data
+                            : false,
+                        onTap: () {
+                          if (isMobile) {
+                            Navigator.of(context).pop();
+                            GoRouter.of(context).go('/settings/data');
+                          } else {
+                            setState(() {
+                              _view = SettingsView.data;
+                            });
+                          }
+                        },
+                      ),
+                      ListTile(
+                        title: Text(AppLocalizations.of(context)!.behaviors),
+                        leading: const Icon(PhosphorIcons.fadersLight),
+                        selected: widget.isDialog && !isMobile
+                            ? _view == SettingsView.behaviors
+                            : false,
+                        onTap: () {
+                          if (isMobile) {
+                            Navigator.of(context).pop();
+                            GoRouter.of(context).go('/settings/behaviors');
+                          } else {
+                            setState(() {
+                              _view = SettingsView.behaviors;
+                            });
+                          }
+                        },
+                      ),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.monitorLight),
+                          title: Text(
+                              AppLocalizations.of(context)!.personalization),
+                          selected: widget.isDialog && !isMobile
+                              ? _view == SettingsView.personalization
+                              : false,
+                          onTap: () {
+                            if (isMobile) {
+                              Navigator.of(context).pop();
+                              GoRouter.of(context)
+                                  .go('/settings/personalization');
+                            } else {
+                              setState(() {
+                                _view = SettingsView.personalization;
+                              });
+                            }
+                          }),
+                      const Divider(),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.articleLight),
+                          title:
+                              Text(AppLocalizations.of(context)!.documentation),
+                          onTap: () => launchUrl(
+                              Uri.https('docs.butterfly.linwood.dev', ''))),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.flagLight),
+                          title:
+                              Text(AppLocalizations.of(context)!.releaseNotes),
+                          onTap: () => openReleaseNotes()),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.usersLight),
+                          title: const Text('Discord'),
+                          onTap: () => launchUrl(
+                              Uri.https('go.linwood.dev', 'discord'))),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.translateLight),
+                          title: const Text('Crowdin'),
+                          onTap: () => launchUrl(Uri.https(
+                              'go.linwood.dev', 'butterfly/crowdin'))),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.codeLight),
+                          title: Text(AppLocalizations.of(context)!.source),
+                          onTap: () => launchUrl(
+                              Uri.https('go.linwood.dev', 'butterfly/source'))),
+                      ListTile(
+                          leading: const Icon(
+                              PhosphorIcons.arrowCounterClockwiseLight),
+                          title: Text(AppLocalizations.of(context)!.changelog),
+                          onTap: () => launchUrl(Uri.https(
+                              'docs.butterfly.linwood.dev', 'changelog'))),
+                      const Divider(),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.stackLight),
+                          title: Text(AppLocalizations.of(context)!.license),
+                          onTap: () => launchUrl(Uri.https(
+                              'go.linwood.dev', 'butterfly/license'))),
+                      ListTile(
+                          leading:
+                              const Icon(PhosphorIcons.identificationCardLight),
+                          title: Text(AppLocalizations.of(context)!.imprint),
+                          onTap: () => launchUrl(
+                              Uri.https('go.linwood.dev', 'impress'))),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.shieldLight),
+                          title:
+                              Text(AppLocalizations.of(context)!.privacypolicy),
+                          onTap: () => launchUrl(Uri.https(
+                              'docs.butterfly.linwood.dev', 'privacypolicy'))),
+                      ListTile(
+                          leading: const Icon(PhosphorIcons.infoLight),
+                          title:
+                              Text(AppLocalizations.of(context)!.information),
+                          onTap: () => PackageInfo.fromPlatform().then((info) =>
+                              showAboutDialog(
+                                  context: context,
+                                  children: [
+                                    ElevatedButton(
+                                        child: Text(
+                                            AppLocalizations.of(context)!
+                                                .copyVersion),
+                                        onPressed: () => Clipboard.setData(
+                                            ClipboardData(text: info.version))),
+                                  ],
+                                  applicationVersion: info.version,
+                                  applicationIcon: Image.asset(
+                                      'images/logo.png',
+                                      height: 50)))),
+                      if (kIsWeb)
+                        Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: InkWell(
+                                onTap: () => launchUrl(Uri.https(
+                                        'vercel.com', '', {
+                                      'utm_source': 'Linwood',
+                                      'utm_campaign': 'oss'
+                                    })),
+                                child: Material(
+                                  child: SizedBox(
+                                      height: 50,
+                                      child: SvgPicture.asset(
+                                          'images/powered-by-vercel.svg',
+                                          placeholderBuilder: (BuildContext
+                                                  context) =>
+                                              Container(
+                                                  padding: const EdgeInsets.all(
+                                                      30.0),
+                                                  child:
+                                                      const CircularProgressIndicator()),
+                                          semanticsLabel: 'Powered by Vercel')),
+                                )))
+                    ]),
+              ),
+            )
+          ]);
+          if (isMobile) {
+            return navigation;
+          }
+          Widget content;
+          switch (_view) {
+            case SettingsView.data:
+              content = const DataSettingsPage(inView: true);
+              break;
+            case SettingsView.behaviors:
+              content = const BehaviorsSettingsPage(inView: true);
+              break;
+            case SettingsView.personalization:
+              content = const PersonalizationSettingsPage(inView: true);
+              break;
+          }
+          return Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            SizedBox(width: 300, child: navigation),
+            Expanded(child: content),
+          ]);
+        }),
+      ),
     );
   }
 }
