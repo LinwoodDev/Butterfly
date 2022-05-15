965c47a5779e0d528c84285f56d9d9292935bd7c
CodeDoctorDE <wusel905@gmail.com>
2022-05-15 10:22:06 +0200

    Migrate to react-animation-on-scroll

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -6,14 +6,14 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from "../components/HomepageFeatures";
 import Translate from '@docusaurus/Translate';
-import AOS from 'simple-aos';
+import "animate.css/animate.min.css";
 import UAParser from 'ua-parser-js';
-
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function HomepageHeader() {
   const [platform, setPlatform] = React.useState('');
-  const parser = new UAParser();
   React.useEffect(() => {
+    const parser = new UAParser();
     const uaPlatform = parser.getOS().name;
     // Test if it is a linux distro
     if (['Raspbian', 'Debian', 'Ubuntu', 'Linux Mint', 'Fedora', 'Arch', 'CentOS', 'Red Hat', 'Manjaro'].includes(uaPlatform)) {
@@ -21,10 +21,8 @@ function HomepageHeader() {
     } else {
       setPlatform(uaPlatform);
     }
-
-    AOS.init();
   }, []);
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   const Svg = require('../../static/img/logo.svg').default;
   return (
     <header className={clsx('hero shadow--lw', styles.heroBanner)}>
@@ -37,55 +35,60 @@ function HomepageHeader() {
           {siteConfig.tagline}
         </p>
         <div className={styles.indexCtas}>
-          <Link data-aos="fade-right"
+          <AnimationOnScroll animateIn="animate__fadeInLeft">
+            <Link data-aos="fade-right"
               className="button button--secondary button--lg"
               to="/docs/1.1/intro">
               <Translate description="homepage getting started button">
-                  Getting started
+                Getting started
               </Translate>
-          </Link>
+            </Link>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeInUp">
             {platform === 'Windows' &&
-              <Link data-aos="fade-left"
-                  className="button button--success button--lg"
-                  to="/downloads/windows">
-                  <Translate description="homepage windows button">
-                      Download for Windows
-                  </Translate>
+              <Link
+                className="button button--success button--lg"
+                to="/downloads/windows">
+                <Translate description="homepage windows button">
+                  Download for Windows
+                </Translate>
               </Link>
             }
             {platform === 'Linux' &&
-              <Link data-aos="fade-left"
-                  className="button button--success button--lg"
-                  to="/downloads/linux">
-                  <Translate description="homepage linux button">
-                      Download for Linux
-                  </Translate>
+              <Link
+                className="button button--success button--lg"
+                to="/downloads/linux">
+                <Translate description="homepage linux button">
+                  Download for Linux
+                </Translate>
               </Link>
             }
             {platform === 'Android' &&
-              <Link data-aos="fade-left"
-                  className="button button--success button--lg"
-                  to="/downloads/android">
-                  <Translate description="homepage android button">
-                      Download for Android
-                  </Translate>
+              <Link
+                className="button button--success button--lg"
+                to="/downloads/android">
+                <Translate description="homepage android button">
+                  Download for Android
+                </Translate>
               </Link>
             }
-            <Link data-aos="fade-left"
-                className="button button--warning button--lg"
-                to="https://butterfly.linwood.dev">
-                  <Translate description="homepage web button">
-                      Open the web app
-                  </Translate>
-                </Link>
-
-            <Link data-aos="fade-left"
-                className="button button--info button--lg"
-                to="/downloads">
-                <Translate description="homepage downloads button">
-                    Downloads
-                </Translate>
+            <Link
+              className="button button--warning button--lg"
+              to="https://butterfly.linwood.dev">
+              <Translate description="homepage web button">
+                Open the web app
+              </Translate>
             </Link>
+          </AnimationOnScroll>
+          <AnimationOnScroll animateIn="animate__fadeInRight">
+            <Link
+              className="button button--info button--lg"
+              to="/downloads">
+              <Translate description="homepage downloads button">
+                Downloads
+              </Translate>
+            </Link>
+          </AnimationOnScroll>
         </div>
       </div>
     </header>
@@ -93,7 +96,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
