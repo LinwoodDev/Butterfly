483e0a80c3118a1c495ccd088c6118c597d32848
CodeDoctorDE <wusel905@gmail.com>
2022-07-01 09:10:23 +0200

    Fix set_version to not include prerelesaes

--- tools/set_version.dart
+++ tools/set_version.dart
@@ -110,8 +110,7 @@ Future<void> updateAppData(String version) async {
   var file = File(
       'app/linux/debian/usr/share/metainfo/dev.linwood.butterfly.appdata.xml');
   if (isPreRelease(version)) {
-    file = File(
-        'app/linux/debian/usr/share/metainfo/dev.linwood.butterfly.appdata.xml.prerelease');
+    return;
   }
   var currentDate = DateTime.now();
   var dateString = DateFormat('yyyy-MM-dd').format(currentDate);
