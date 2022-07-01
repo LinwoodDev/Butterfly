b6dd519d01cf41929cf17942ba53ddf35927e4bd
CodeDoctorDE <wusel905@gmail.com>
2022-07-01 08:05:44 +0200

    Add yaru and adwaita theme

--- app/lib/theme/manager.dart
+++ app/lib/theme/manager.dart
@@ -1,14 +1,25 @@
+import 'package:adwaita/adwaita.dart';
 import 'package:butterfly/theme/themes.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:yaru/yaru.dart';
 
 class ThemeManager {
   static List<String> getThemes() => List<String>.from(kCustomSchemes.keys)
+    ..addAll(['adwaita'])
+    ..addAll(getYaruThemes())
     ..addAll(FlexScheme.values.map((e) => e.name))
     ..remove('custom');
   static ThemeData getThemeByName(String name, {bool dark = false}) {
     FlexSchemeColor? colorScheme;
     if (name.isEmpty) name = 'classic';
+    if (name == 'adwaita') {
+      return getAdwaitaTheme(dark: dark);
+    }
+    final yaruTheme = getYaruTheme(variant: name, dark: dark);
+    if (yaruTheme != null) {
+      return yaruTheme;
+    }
     if (kCustomSchemes.containsKey(name)) {
       colorScheme =
           dark ? kCustomSchemes[name]!.dark : kCustomSchemes[name]!.light;
@@ -41,3 +52,27 @@ class ThemeManager {
     }
   }
 }
+
+ThemeData getAdwaitaTheme({bool dark = false}) {
+  return dark ? AdwaitaThemeData.dark() : AdwaitaThemeData.light();
+}
+
+ThemeData? getYaruTheme({bool dark = false, required String variant}) {
+  try {
+    if (!variant.startsWith('yaru')) {
+      return null;
+    }
+    variant = variant.substring(4);
+    variant = variant[0].toLowerCase() + variant.substring(1);
+    YaruVariant yaruVariant = YaruVariant.values.byName(variant);
+    return dark ? yaruVariant.darkTheme : yaruVariant.theme;
+  } catch (_) {
+    return null;
+  }
+}
+
+List<String> getYaruThemes() => YaruVariant.values
+    .map((e) => e.name)
+    .map((e) => e.substring(0, 1).toUpperCase() + e.substring(1))
+    .map((e) => 'yaru$e')
+    .toList();
