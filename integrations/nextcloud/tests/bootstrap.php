bce9466bcbc070075914fd5e125f9d5faa46514a
CodeDoctorDE <wusel905@gmail.com>
2022-08-14 17:32:11 +0200

    Init nextcloud project

---
+++ integrations/nextcloud/tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('PHPUNIT_RUN')) {
+    define('PHPUNIT_RUN', 1);
+}
+
+require_once __DIR__.'/../../../lib/base.php';
+
+// Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
+\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+
+// Fix for "Autoload path not allowed: .../butterfly/tests/testcase.php"
+\OC_App::loadApp('butterfly');
+
+if(!class_exists('PHPUnit_Framework_TestCase')) {
+    require_once('PHPUnit/Autoload.php');
+}
+
+OC_Hook::clear();
