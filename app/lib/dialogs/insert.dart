092fc28da45544e40f788fd7de58e7180a58626e
CodeDoctorDE <wusel905@gmail.com>
2022-08-14 15:24:59 +0200

    Use import service in insert dialog

--- app/lib/dialogs/insert.dart
+++ app/lib/dialogs/insert.dart
@@ -6,6 +6,7 @@ import 'package:butterfly/bloc/document_bloc.dart';
 import 'package:butterfly/dialogs/camera.dart';
 import 'package:butterfly/dialogs/pages.dart';
 import 'package:butterfly/models/element.dart';
+import 'package:butterfly/services/import.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -28,41 +29,9 @@ class InsertDialog extends StatefulWidget {
 }
 
 class _InsertDialogState extends State<InsertDialog> {
-  void _submit(List<PadElement> elements) {
-    var bloc = context.read<DocumentBloc>();
-    bloc.add(ElementsCreated(elements));
-    bloc.bake();
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _insertImage(Uint8List content) async {
-    _runInsertImage(content).catchError((error, stackTrace) => showDialog(
-        context: context,
-        builder: (context) =>
-            ErrorDialog(error: error, stackTrace: stackTrace)));
-  }
-
-  Future<void> _runInsertImage(Uint8List content) async {
-    var bloc = context.read<DocumentBloc>();
-    var codec = await ui.instantiateImageCodec(content);
-    var frame = await codec.getNextFrame();
-    var image = frame.image.clone();
-
-    var bytes = await image.toByteData(format: ui.ImageByteFormat.png);
-    var state = bloc.state;
-    if (state is! DocumentLoadSuccess) return;
-    _submit([
-      ImageElement(
-          height: image.height,
-          width: image.width,
-          layer: state.currentLayer,
-          pixels: bytes?.buffer.asUint8List() ?? Uint8List(0),
-          position: widget.position)
-    ]);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final importService = context.read<ImportService>();
     return AlertDialog(
       title: Row(
         children: [
@@ -87,7 +56,7 @@ class _InsertDialogState extends State<InsertDialog> {
             if (!kIsWeb) {
               content = await File(e.path ?? '').readAsBytes();
             }
-            _insertImage(content);
+            importService.importImage(content);
           },
         ),
         if (kIsWeb ||
@@ -102,7 +71,7 @@ class _InsertDialogState extends State<InsertDialog> {
                   MaterialPageRoute(builder: (context) => const CameraDialog()),
                 ) as Uint8List?;
                 if (content == null) return;
-                _insertImage(content);
+                importService.importImage(content);
               }),
         ListTile(
           title: Text(AppLocalizations.of(context)!.svg),
@@ -119,83 +88,25 @@ class _InsertDialogState extends State<InsertDialog> {
             if (!kIsWeb) {
               content = await File(e.path ?? '').readAsBytes();
             }
-            var contentString = String.fromCharCodes(content);
-            final SvgParser parser = SvgParser();
-            try {
-              var document = await parser.parse(contentString,
-                  warningsAsErrors: true, key: contentString);
-              final size = document.viewport.viewBox;
-              var height = size.height, width = size.width;
-              if (!height.isFinite) height = 0;
-              if (!width.isFinite) width = 0;
-              var elements = [
-                SvgElement(
-                  width: width,
-                  height: height,
-                  data: contentString,
-                  position: widget.position,
-                ),
-              ];
-              _submit(elements);
-            } catch (e, stackTrace) {
-              if (mounted) {
-                showDialog(
-                    context: context,
-                    builder: (context) => ErrorDialog(
-                          error: e,
-                          stackTrace: stackTrace,
-                        ));
-              }
-            }
+            importService.importSvg(content);
           },
         ),
         ListTile(
             title: Text(AppLocalizations.of(context)!.pdf),
             leading: const Icon(PhosphorIcons.filePdfLight),
             onTap: () async {
-              try {
-                final files = await FilePicker.platform.pickFiles(
-                    type: FileType.custom,
-                    allowedExtensions: ['pdf'],
-                    allowMultiple: false,
-                    withData: true);
-                if (files?.files.isEmpty ?? true) return;
-                var e = files!.files.first;
-                var content = e.bytes ?? Uint8List(0);
-                if (!kIsWeb) {
-                  content = await File(e.path ?? '').readAsBytes();
-                }
-                final elements = <Uint8List>[];
-                await for (var page in Printing.raster(content)) {
-                  final png = await page.toPng();
-                  elements.add(png);
-                }
-                final callback = await showDialog<PageDialogCallback>(
-                    context: context,
-                    builder: (context) => PagesDialog(pages: elements));
-                if (callback == null) return;
-                final selectedElements = <ImageElement>[];
-                var y = widget.position.dy;
-                await for (var page in Printing.raster(content,
-                    pages: callback.pages,
-                    dpi: PdfPageFormat.inch * callback.quality)) {
-                  final png = await page.toPng();
-                  final scale = 1 / callback.quality;
-                  selectedElements.add(ImageElement(
-                      height: page.height,
-                      width: page.width,
-                      pixels: png,
-                      constraints: ElementConstraints.scaled(scale),
-                      position: Offset(widget.position.dx, y)));
-                  y += page.height;
-                }
-                _submit(selectedElements);
-              } catch (e, stackTrace) {
-                showDialog(
-                    context: context,
-                    builder: (context) =>
-                        ErrorDialog(error: e, stackTrace: stackTrace));
+              final files = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: ['pdf'],
+                  allowMultiple: false,
+                  withData: true);
+              if (files?.files.isEmpty ?? true) return;
+              var e = files!.files.first;
+              var content = e.bytes ?? Uint8List(0);
+              if (!kIsWeb) {
+                content = await File(e.path ?? '').readAsBytes();
               }
+              importService.importSvg(content);
             }),
       ]),
       actions: [
