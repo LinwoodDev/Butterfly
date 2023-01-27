ea0389e5b1720a6f605618b1e273145622789cd9
CodeDoctorDE <codedoctor@linwood.dev>
2023-01-27 12:00:31 +0100

    Fix theme

--- app/linux/flutter/generated_plugins.cmake
+++ app/linux/flutter/generated_plugins.cmake
@@ -4,11 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_linux
+  gtk
   printing
   screen_retriever
   url_launcher_linux
   window_manager
-  yaru
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
