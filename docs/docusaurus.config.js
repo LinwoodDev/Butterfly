d4c2b74be9edad84518d07a68d3b873ea72e2b67
CodeDoctorDE <codedoctor@linwood.dev>
2023-03-13 09:10:12 +0100

    Add swc-loader

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -281,5 +281,23 @@ module.exports = {
             },
         ],
         // Other tweaks
-    ]
+    ],
+    
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
+      },
+    }),
+  },
 };
