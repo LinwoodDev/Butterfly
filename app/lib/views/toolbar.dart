327dbe868b9f44318e05050d2dff5024a0501989
CodeDoctorDE <codedoctor@linwood.dev>
2023-06-21 19:42:19 +0200

    Upgrade to docusaurus 3

--- app/lib/views/toolbar.dart
+++ app/lib/views/toolbar.dart
@@ -1,4 +1,3 @@
-import 'package:butterfly/bloc/document_bloc.dart';
 import 'package:butterfly/cubits/current_index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
