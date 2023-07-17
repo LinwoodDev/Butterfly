76af2aa2d6c83a4fcd13b5a1e0e802ce593d4fca
CodeDoctorDE <codedoctor@linwood.dev>
2023-07-17 19:42:31 +0200

    Add paste context menu

--- app/lib/dialogs/elements.dart
+++ app/lib/dialogs/elements.dart
@@ -1,3 +1,4 @@
+import 'package:butterfly/actions/paste.dart';
 import 'package:butterfly/cubits/current_index.dart';
 import 'package:butterfly/handlers/handler.dart';
 import 'package:butterfly/visualizer/event.dart';
@@ -24,81 +25,93 @@ class ElementsDialog extends StatelessWidget {
           child: ListView(
         shrinkWrap: true,
         children: [
-          MenuItemButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              context
-                  .read<CurrentIndexCubit>()
-                  .fetchHandler<HandHandler>()
-                  ?.copySelection(context, true);
-            },
-            leadingIcon: const PhosphorIcon(PhosphorIconsLight.scissors),
-            child: Text(AppLocalizations.of(context).cut),
-          ),
-          MenuItemButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              context
-                  .read<CurrentIndexCubit>()
-                  .fetchHandler<HandHandler>()
-                  ?.copySelection(context, false);
-            },
-            leadingIcon: const PhosphorIcon(PhosphorIconsLight.copy),
-            child: Text(AppLocalizations.of(context).copy),
-          ),
-          MenuItemButton(
-            leadingIcon: const PhosphorIcon(PhosphorIconsLight.copy),
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              context
-                  .read<CurrentIndexCubit>()
-                  .fetchHandler<HandHandler>()
-                  ?.transform(
-                      context.read<DocumentBloc>(),
-                      renderers
-                          .map((e) => Renderer.fromInstance(e.element))
-                          .toList(),
-                      null,
-                      true);
-            },
-            child: Text(AppLocalizations.of(context).duplicate),
-          ),
-          MenuItemButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              context.read<DocumentBloc>().add(
-                  ElementsRemoved(renderers.map((r) => r.element).toList()));
-            },
-            leadingIcon: const PhosphorIcon(PhosphorIconsLight.trash),
-            child: Text(AppLocalizations.of(context).delete),
-          ),
-          SubmenuButton(
-            leadingIcon: const Icon(PhosphorIconsLight.layout),
-            menuStyle: const MenuStyle(alignment: Alignment.centerRight),
-            menuChildren: Arrangement.values
-                .map((e) => MenuItemButton(
-                      leadingIcon: Icon(e.icon(PhosphorIconsStyle.light)),
-                      child: Text(e.getLocalizedName(context)),
-                      onPressed: () {
-                        Navigator.of(context).pop(true);
-                        context.read<DocumentBloc>().add(ElementsArranged(
-                            renderers.map((r) => r.element).toList(), e));
-                      },
-                    ))
-                .toList(),
-            child: Text(AppLocalizations.of(context).arrange),
-          ),
-          MenuItemButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              if (renderers.isEmpty) return;
-              final cubit = context.read<CurrentIndexCubit>();
-              cubit.changeSelection(renderers.first);
-              renderers.sublist(1).forEach((r) => cubit.insertSelection(r));
-            },
-            leadingIcon: const PhosphorIcon(PhosphorIconsLight.faders),
-            child: Text(AppLocalizations.of(context).properties),
-          ),
+          if (renderers.isEmpty) ...[
+            MenuItemButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                Actions.invoke(
+                    context, const PasteTextIntent(SelectionChangedCause.tap));
+              },
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.clipboard),
+              child: Text(AppLocalizations.of(context).paste),
+            ),
+          ] else ...[
+            MenuItemButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                context
+                    .read<CurrentIndexCubit>()
+                    .fetchHandler<HandHandler>()
+                    ?.copySelection(context, true);
+              },
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.scissors),
+              child: Text(AppLocalizations.of(context).cut),
+            ),
+            MenuItemButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                context
+                    .read<CurrentIndexCubit>()
+                    .fetchHandler<HandHandler>()
+                    ?.copySelection(context, false);
+              },
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.copy),
+              child: Text(AppLocalizations.of(context).copy),
+            ),
+            MenuItemButton(
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.copy),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                context
+                    .read<CurrentIndexCubit>()
+                    .fetchHandler<HandHandler>()
+                    ?.transform(
+                        context.read<DocumentBloc>(),
+                        renderers
+                            .map((e) => Renderer.fromInstance(e.element))
+                            .toList(),
+                        null,
+                        true);
+              },
+              child: Text(AppLocalizations.of(context).duplicate),
+            ),
+            MenuItemButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                context.read<DocumentBloc>().add(
+                    ElementsRemoved(renderers.map((r) => r.element).toList()));
+              },
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.trash),
+              child: Text(AppLocalizations.of(context).delete),
+            ),
+            SubmenuButton(
+              leadingIcon: const Icon(PhosphorIconsLight.layout),
+              menuStyle: const MenuStyle(alignment: Alignment.centerRight),
+              menuChildren: Arrangement.values
+                  .map((e) => MenuItemButton(
+                        leadingIcon: Icon(e.icon(PhosphorIconsStyle.light)),
+                        child: Text(e.getLocalizedName(context)),
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                          context.read<DocumentBloc>().add(ElementsArranged(
+                              renderers.map((r) => r.element).toList(), e));
+                        },
+                      ))
+                  .toList(),
+              child: Text(AppLocalizations.of(context).arrange),
+            ),
+            MenuItemButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+                if (renderers.isEmpty) return;
+                final cubit = context.read<CurrentIndexCubit>();
+                cubit.changeSelection(renderers.first);
+                renderers.sublist(1).forEach((r) => cubit.insertSelection(r));
+              },
+              leadingIcon: const PhosphorIcon(PhosphorIconsLight.faders),
+              child: Text(AppLocalizations.of(context).properties),
+            ),
+          ],
         ],
       ))
     ]);
