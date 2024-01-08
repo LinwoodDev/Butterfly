4233a4d463981b5eb958e44be1272494ae8e0f48
CodeDoctorDE <codedoctor@linwood.dev>
2024-01-08 11:23:28 +0100

    Fix exporting on mobile devices

--- app/lib/api/save.dart
+++ app/lib/api/save.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:file_selector/file_selector.dart' as fs;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:share_plus/share_plus.dart';
 
 Future<void> exportFile(
   BuildContext context,
@@ -9,6 +13,12 @@ Future<void> exportFile(
   String fileExtension,
   String mimeType,
 ) async {
+  final file = fs.XFile.fromData(Uint8List.fromList(bytes),
+      mimeType: mimeType, name: 'output.$fileExtension');
+  if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+    Share.shareXFiles([file]);
+    return;
+  }
   final result = await fs.getSaveLocation(
     acceptedTypeGroups: [
       fs.XTypeGroup(
@@ -19,8 +29,6 @@ Future<void> exportFile(
     ],
   );
   if (result == null) return;
-  final file = fs.XFile.fromData(Uint8List.fromList(bytes),
-      mimeType: mimeType, name: 'output.$fileExtension');
   await file.saveTo(result.path);
 }
 
