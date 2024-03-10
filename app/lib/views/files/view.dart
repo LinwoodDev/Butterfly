2e0197cb7897e778e2267f359bbb3f587290ee7c
CodeDoctorDE <codedoctor@linwood.dev>
2024-03-10 10:54:48 +0100

    Fix file creation if the file path already exists

--- app/lib/views/files/view.dart
+++ app/lib/views/files/view.dart
@@ -81,7 +81,6 @@ class FilesViewState extends State<FilesView> {
     _filesStream = _fileSystem.fetchAsset(_locationController.text);
   }
 
-  // ! modifi with for error setState() called after dispose(), during big file import, check if the widget is still mounted before calling setState()
   void reloadFileSystem() {
     if (mounted) {
       setState(_setFilesStream);
