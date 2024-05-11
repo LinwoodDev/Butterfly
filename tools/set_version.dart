54b3ca749cb56bf0b3da737c59141c301404be8a
CodeDoctorDE <codedoctor@linwood.dev>
2024-05-11 11:03:57 +0200

    Fix linux metainfo

--- tools/set_version.dart
+++ tools/set_version.dart
@@ -109,7 +109,7 @@ Future<void> updateAppData(String version) async {
   var dateString = DateFormat('yyyy-MM-dd').format(currentDate);
   var line = '\t\t<release version="$version" date="$dateString" />';
   var lines = List<String>.from(await file.readAsLines());
-  lines.insert(41, line);
+  lines.insert(40, line);
   await file.writeAsString(lines.join('\n'));
   print('Successfully updated appdata version to $version');
 }
