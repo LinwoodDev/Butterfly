e54dbf62460fadc1529375de50af9002306b6ff5
CodeDoctorDE <codedoctor@linwood.dev>
2024-06-24 21:20:47 +0200

    Bump versions

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -391,4 +391,4 @@ const isNightly =
     flavor == 'nightly' || flavor == 'dev' || flavor == 'development';
 const shortApplicationName = isNightly ? 'Butterfly Nightly' : 'Butterfly';
 const applicationName = 'Linwood $shortApplicationName';
-const applicationMinorVersion = '2.1';
+const applicationMinorVersion = '2.2';
