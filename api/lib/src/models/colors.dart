90110c2a13d5c367bc7eec04cf06b7c4fd956a45
CodeDoctorDE <codedoctor@linwood.dev>
2024-12-13 20:01:35 +0100

    Use material blue and red as default colors

--- api/lib/src/models/colors.dart
+++ api/lib/src/models/colors.dart
@@ -3,8 +3,8 @@ class BasicColors {
   static const light = 0xFFD9D9D9;
   static const black = 0xFF000000;
   static const dark = 0xFF1A1A1A;
-  static const red = 0xFFFF0000;
-  static const blue = 0xFF0000FF;
+  static const red = 0xFFF44336;
+  static const blue = 0xFF2196F3;
   static const transparent = 0x00000000;
 
   const BasicColors._();
