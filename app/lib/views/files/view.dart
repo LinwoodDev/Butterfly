e483ba712c66bb11d5848e5c437f15cc45a9c47c
CodeDoctorDE <codedoctor@linwood.dev>
2025-04-13 15:21:42 +0200

    Use opened document parent directory as initial path in file navigator

--- app/lib/views/files/view.dart
+++ app/lib/views/files/view.dart
@@ -26,9 +26,11 @@ class FilesView extends StatefulWidget {
   final ValueChanged<FileSystemFile<NoteFile>>? onPreview;
   final bool collapsed;
   final bool isMobile, isPage;
+  final String? initialPath;
 
   const FilesView({
     super.key,
+    this.initialPath,
     this.activeAsset,
     this.remote,
     this.onRemoteChanged,
@@ -66,6 +68,10 @@ class FilesViewState extends State<FilesView> {
     _sortBy = _settingsCubit.state.sortBy;
     _sortOrder = _settingsCubit.state.sortOrder;
     _remote = widget.remote ?? _settingsCubit.getRemote();
+    final initialPath = widget.initialPath;
+    if (initialPath != null) {
+      _locationController.text = initialPath;
+    }
     _setFilesStream();
   }
 
