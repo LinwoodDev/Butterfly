772f24c0286f33de766e32e126ead999c95c480f
CodeDoctorDE <codedoctor@linwood.dev>
2025-05-05 21:42:24 +0200

    Fix flavor in build gradle kts

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -28,12 +28,30 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    flavorDimensions += "default"
+    productFlavors {
+        create("production") {
+            dimension = "default"
+            applicationIdSuffix = ""
+            manifestPlaceholders["appName"] = "Butterfly"
+        }
+        create("development") {
+            dimension = "default"
+            applicationIdSuffix = ""
+            manifestPlaceholders["appName"] = "Butterfly Nightly"
+        }
+        create("nightly") {
+            dimension = "default"
+            applicationIdSuffix = ".nightly"
+            manifestPlaceholders["appName"] = "Butterfly Nightly"
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "dev.linwood.butterfly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
