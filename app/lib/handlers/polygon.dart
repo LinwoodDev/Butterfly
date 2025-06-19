c4d1cb9db177598650b213f48c6f5d57069b4bd2
CodeDoctorDE <codedoctor@linwood.dev>
2025-06-19 22:37:49 +0200

    Add basic handler and renderer for path

--- app/lib/handlers/polygon.dart
+++ app/lib/handlers/polygon.dart
@@ -1,7 +1,18 @@
 part of 'handler.dart';
 
 class PolygonHandler extends Handler<PolygonTool> with ColoredHandler {
+  PolygonElement? _element;
+
   PolygonHandler(super.data);
+
+  @override
+  List<Renderer> createForegrounds(
+    CurrentIndexCubit currentIndexCubit,
+    NoteData document,
+    DocumentPage page,
+    DocumentInfo info, [
+    Area? currentArea,
+  ]) => [if (_element != null) PolygonRenderer(_element!)];
 
   @override
   SRGBColor getColor() => data.property.color;
@@ -9,4 +20,21 @@ class PolygonHandler extends Handler<PolygonTool> with ColoredHandler {
   @override
   PolygonTool setColor(SRGBColor color) =>
       data.copyWith(property: data.property.copyWith(color: color));
+
+  void addPoint(EventContext context, Offset localPos) {
+    final globalPos = context.getCameraTransform().localToGlobal(localPos);
+    var element =
+        _element ?? PolygonElement(points: [], property: data.property);
+    element = element.copyWith(
+      points: [...element.points, PolygonPoint(globalPos.dx, globalPos.dy)],
+    );
+    _element = element;
+    context.refresh();
+  }
+
+  @override
+  void onTapUp(TapUpDetails details, EventContext context) {
+    final localPos = details.localPosition;
+    addPoint(context, localPos);
+  }
 }
--- app/lib/renderers/elements/polygon.dart
+++ app/lib/renderers/elements/polygon.dart
@@ -14,6 +14,44 @@ class PolygonRenderer extends Renderer<PolygonElement> {
     ColorScheme? colorScheme,
     bool foreground = false,
   ]) {
-    // TODO: implement build
+    final points = element.points;
+    if (points.isEmpty) return;
+    final path = ui.Path();
+    final first = points.first;
+    path.moveTo(first.x, first.y);
+    for (var i = 1; i < points.length; i++) {
+      final point = points[i];
+      final prev = points[i - 1];
+
+      if (prev.handleOut != null || point.handleIn != null) {
+        path.cubicTo(
+          prev.handleOut?.x ?? prev.x,
+          prev.handleOut?.y ?? prev.y,
+          point.handleIn?.x ?? point.x,
+          point.handleIn?.y ?? point.y,
+          point.x,
+          point.y,
+        );
+      } else {
+        path.lineTo(point.x, point.y);
+      }
+    }
+    final property = element.property;
+
+    if (property.color.a > 0) {
+      final paint = Paint()
+        ..color = property.color.toColor()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = property.strokeWidth
+        ..strokeCap = StrokeCap.round
+        ..strokeJoin = StrokeJoin.round;
+      canvas.drawPath(path, paint);
+    }
+    if (property.fill.a > 0) {
+      final fillPaint = Paint()
+        ..color = property.fill.toColor()
+        ..style = PaintingStyle.fill;
+      canvas.drawPath(path, fillPaint);
+    }
   }
 }
