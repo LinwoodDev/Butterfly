b828b00619d1dddcdf8d71c41c3b28d6849c5e12
CodeDoctorDE <codedoctor@linwood.dev>
2025-08-16 23:08:13 +0200

    Upgrade to flutter 3.35

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         applicationId = "dev.linwood.butterfly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
