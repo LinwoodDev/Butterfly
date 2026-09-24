b49f6ee0080c5ddfcabbdf5164660a475c1fe974
CodeDoctorDE <codedoctor@linwood.dev>
2026-09-24 13:32:24 +0200

    Migrate Android app to built-in Kotlin

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -4,8 +4,7 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after the Android Gradle plugin.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
